f00a1566639ed67a1fb63b1c32ca40c393a7eca2
Tomasz Tarkowski <Tomasz.Tarkowski@fuw.edu.pl>
2020-11-25 14:54:48 +0100

    New example (maximum of function R -> R -> R).

---
+++ examples/02/example.cc
@@ -0,0 +1,62 @@
+// Evolutionary search for a maximum of given function over domain
+// - function: f(x, y) = cos(0.25 * r(x, y)) + e
+// - domain: [-10, +10] x [-10, +10]
+// - representation: floating-point
+// - variation type: Gaussian mutation (fixed sigma), no recombination
+// - parents/surivor selection: stochastic universal sampling (SUS)
+// - termination condition: based on maximum fitness improvement
+
+#include <cmath>
+#include <cstddef>
+#include <numbers>
+#include <fstream>
+#include <quile/quile.h>
+
+using namespace quile;
+
+using type = double;
+
+// Function
+const auto f =
+  [](type x, type y) -> fitness {
+    const auto r = [](type x, type y) -> type {
+                     return std::sqrt(x * x + y * y);
+                   };
+    return std::cos(0.25 * r(x, y)) + std::numbers::e_v<fitness>;
+  };
+
+// Domain
+const domain<type, 2> d{range{-10., +10.}, range{-10., +10.}};
+
+int main() {
+  using G = genotype<type, 2, &d>;
+  const fitness_function<G> ff =
+    [](const G& g) { return f(g.value(0), g.value(1)); };
+  const fitness_db<G> fd{ff, constraints_satisfied<G>};
+  const fitness_proportional_selection<G> fps{fd};
+
+  // First generation creator
+  const auto p0 =
+    random_population<constraints_satisfied<G>, G>{};
+  // Parents selection 
+  const auto p1 = stochastic_universal_sampling<G>{fps};
+  // Survivor selection
+  const auto p2 = adapter<G>(stochastic_universal_sampling<G>{fps});
+
+  const std::size_t generation_sz{1000};
+  const std::size_t parents_sz{42};
+  const auto tc = max_fitness_improvement_termination<G>(fd, 10, 0.05);
+
+  const type sigma{.2};
+  const variation<G> v{Gaussian_mutation<G>(sigma)};
+  
+  std::ofstream file{"evolution.dat"};
+  for (std::size_t i = 0;
+       const auto& x :
+         evolution<G>(v, p0, p1, p2, tc, generation_sz, parents_sz)) {
+    for (const auto& xx : x) {
+      file << i << ' ' << xx.value(0) << ' ' << xx.value(1) << '\n';
+    }
+    ++i;
+  }
+}
---
+++ examples/02/postprocessing.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+file=evolution.dat
+temp=temp.dat
+out=plot.png
+
+for i in `cat evolution.dat | awk '{ print $1 }' | uniq`
+do
+  cat ${file} | grep "^${i} " > ${temp}
+  echo "\
+    reset; \
+    set samples 1000, 1000; \
+    set xrange [-10: 10]; \
+    set yrange [-10: 10]; \
+    r(x, y) = sqrt(x * x + y * y); \
+    f(x, y) = cos(0.25 * r(x, y)) + exp(1.0); \
+    set t png; \
+    set o \"${out}\"; \
+    set title \"${i}\"; \
+    set pm3d; \
+    set cbtics 0.5; \
+    unset border; \
+    unset xtics; \
+    unset ytics; \
+    unset ztics; \
+    set view 60, 350; \
+    splot f(x, y) notitle, \
+          \"${temp}\" u 2:3:(f(\$2, \$3)) pt 7 notitle; \
+    set o" | gnuplot
+  mv ${out} `printf '%03d.png' ${i}`
+done
+
+gst-launch-1.0 -e multifilesrc location="%03d.png" \
+  index=0 caps="image/png,framerate=(fraction)10/1" \
+  ! pngdec ! videoconvert ! theoraenc ! oggmux \
+  ! filesink location=result.ogv
+
+rm ${temp} *.png
+
+exit
+
