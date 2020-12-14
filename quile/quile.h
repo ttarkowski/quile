cc0d892a09f94272902523ed25356c8e2a47576a
Tomasz Tarkowski <Tomasz.Tarkowski@fuw.edu.pl>
2020-12-14 16:41:07 +0100

    TMP typelist removed; set_of_departure concept added.

--- quile/quile.h
+++ quile/quile.h
@@ -55,9 +55,9 @@
 
 namespace quile {
 
-  //////////////////////////////
-  // Template metaprogramming //
-  //////////////////////////////
+  //////////////
+  // TMP loop //
+  //////////////
   
   template<std::integral T, T I, T N>
   struct static_loop {
@@ -71,59 +71,6 @@ namespace quile {
       static_loop<T, I + 1, N>::body(std::forward<decltype(f)>(f));
     }
   };
-  
-  template<typename...>
-  struct typelist;
-  
-  template<typename>
-  struct is_typelist : std::false_type {};
-  
-  template<typename... Ts>
-  struct is_typelist<typelist<Ts...>> : std::true_type {};
-  
-  template<typename T>
-  constexpr inline bool is_typelist_v = is_typelist<T>::value;
-  
-  template<typename...>
-  struct typelist_cat;
-  
-  template<>
-  struct typelist_cat<typelist<>> {
-    using type = typelist<>;
-  };
-  
-  template<typename... Ts>
-  struct typelist_cat<typelist<Ts...>> {
-    using type = typelist<Ts...>;
-  };
-  
-  template<typename... Ts, typename... Us>
-  struct typelist_cat<typelist<Ts...>, typelist<Us...>> {
-    using type = typelist<Ts..., Us...>;
-  };
-  
-  template<typename... Ts, typename... Us, typename... Rest>
-  struct typelist_cat<typelist<Ts...>, typelist<Us...>, Rest...> {
-    using type = typename typelist_cat<typelist<Ts..., Us...>, Rest...>::type;
-  };
-  
-  template<typename... Ts>
-  using typelist_cat_t = typename typelist_cat<Ts...>::type;
-  
-  template<typename T, std::size_t N>
-  struct repeated_type {
-    using type =
-      typename typelist_cat<typelist<T>,
-                            typename repeated_type<T, N - 1>::type>::type;
-  };
-  
-  template<typename T>
-  struct repeated_type<T, 0> {
-    using type = typelist<>;
-  };
-  
-  template<typename T, std::size_t N>
-  using repeated_type_t = typename repeated_type<T, N>::type;
   
   //////////////////////////////////
   // Functional logical operators //
@@ -303,6 +250,18 @@ namespace quile {
   template<typename T, std::size_t N>
   using domain = std::array<range<T>, N>;
   
+  template<typename T>
+  struct is_domain : std::false {};
+  
+  template<typename T, std::size_t N>
+  struct is_domain<domain<T, N>> : std::true {};
+  
+  template<typename T>
+  inline constexpr bool is_domain_v = is_domain<T>::value;
+  
+  template<typename T>
+  concept set_of_departure = is_domain_v<T>;
+  
   template<typename T, std::size_t N>
   bool contains(const domain<T, N>& d, const std::array<T, N>& p) {
     bool res = true;
@@ -311,14 +270,14 @@ namespace quile {
     }
     return res;
   }
-
+  
   template<typename T, std::size_t N>
   constexpr domain<T, N> uniform_domain(const range<T>& r) {
     domain<T, N> res{};
     std::generate_n(std::begin(res), N, [&]() { return r; });
     return res;
   }
-
+  
   template<typename T, std::size_t N>
   constexpr bool uniform(const domain<T, N>& d) {
     const auto x0 = N == 0? range<T>{} : d[0];
@@ -1243,7 +1202,7 @@ namespace quile {
       };
     return population<G>{G{f(g1, g0.data())}, G{f(g0, g1.data())}};
   }
-
+  
 } // namespace quile
 
 #endif // QUILE_H
