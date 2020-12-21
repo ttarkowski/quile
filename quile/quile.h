334cd9e2bc0805f1c1bbbd601d15e226cf313d67
Tomasz Tarkowski <Tomasz.Tarkowski@fuw.edu.pl>
2020-12-21 16:25:25 +0100

    Coordinate systems.

--- quile/quile.h
+++ quile/quile.h
@@ -249,7 +249,7 @@ namespace quile {
   
   template<typename T> requires std::floating_point<T> || std::integral<T>
   T square(T x) { return x * x; }
-
+  
   template<typename T> requires std::floating_point<T> || std::integral<T>
   T cube(T x) { return x * x * x; }
   
@@ -261,6 +261,42 @@ namespace quile {
   
   template<std::floating_point T>
   const T ln2 = std::numbers::ln2_v<T>;
+  
+  namespace detail {
+    
+    template<std::floating_point T>
+    T angle(T x, T y) {
+      const T t = std::atan2(y, x);
+      return t >= T{0} ? t : (2 * std::numbers::pi_v<T> + t);
+    }
+    
+  } // namespace detail
+  
+  template<std::floating_point T>
+  std::tuple<T, T, T> cart2spher(T x, T y, T z) {
+    return std::tuple<T, T, T>{
+      std::hypot(x, y, z),                // r
+      std::acos(z / std::hypot(x, y, z)), // theta
+      detail::angle(x, y)                 // phi
+    };
+  }
+  
+  template<std::floating_point T>
+  std::tuple<T, T, T> spher2cart(T r, T theta, T phi) {
+    return std::tuple<T, T, T>{
+      r * std::sin(theta) * std::cos(phi),
+      r * std::sin(theta) * std::sin(phi),
+      r * std::cos(theta)
+    };
+  }
+  
+  template<std::floating_point T>
+  std::tuple<T, T> cart2polar(T x, T y)
+  { return std::tuple<T, T>{std::hypot(x, y), detail::angle(x, y)}; }
+  
+  template<std::floating_point T>
+  std::tuple<T, T> polar2cart(T r, T phi)
+  { return std::tuple<T, T>{r * std::cos(phi), r * std::sin(phi)}; }
   
   ////////////
   // Domain //
