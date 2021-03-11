7fb4bd7c53082d84eaf7c3446a67814daea505da
Tomasz Tarkowski <Tomasz.Tarkowski@fuw.edu.pl>
2021-03-11 16:43:54 +0100

    Fix.

--- quile/quile.h
+++ quile/quile.h
@@ -1659,10 +1659,11 @@ template<std::floating_point T, std::size_t N>
 const test_function<T, N> stepint{
   "stepint",
   [](const point<T, N>& p) {
-    return std::transform_reduce(
-      std::begin(p), std::end(p), T{ 0. }, std::plus<T>{}, [](auto x) {
-        return std::floor(std::fabs(x));
-      });
+    return 25. +
+           std::transform_reduce(
+             std::begin(p), std::end(p), T{ 0. }, std::plus<T>{}, [](auto x) {
+               return std::floor(std::fabs(x));
+             });
   },
   []() { return uniform_domain<T, N>(-5.12, 5.12); },
   []() { return uniform_point<T, N>(0.); }
