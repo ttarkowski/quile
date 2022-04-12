fbc3b2a286131a18cf0fcabb608bdfa42a639769
Tomasz Tarkowski <Tomasz.Tarkowski.TT@gmail.com>
2022-04-12 14:17:50 +0200

    fn_and example.

---
+++ doc/examples/fn_and.cc
@@ -0,0 +1,9 @@
+#include <cassert>
+#include <quile/quile.h>
+
+int main() {
+  const auto f0 = [](int i) { return i == 42; };
+  const auto f1 = [](int i) { return i % 2 == 1; };
+  const auto f = quile::fn_and(f0, f1);
+  assert(!f(42));
+}
