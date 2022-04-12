b9401b5314e1c421e27a570bf19af4c181810f9a
Tomasz Tarkowski <Tomasz.Tarkowski.TT@gmail.com>
2022-04-12 14:46:42 +0200

    thread_pool example.

---
+++ doc/examples/thread_pool.cc
@@ -0,0 +1,20 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <quile/quile.h>
+
+int main() {
+  using namespace std::chrono_literals;
+  quile::thread_pool tp{4};
+  std::vector<std::future<void>> v0{};
+  for (int i = 0; i < 10; ++i) {
+    v0.push_back(tp.async<void>(std::launch::async, [i]() {
+      std::cout << i << '\n';
+      std::this_thread::sleep_for((i % 2 + 1) * 1s);
+    }));
+  }
+  for (auto& x : v0) {
+    x.get();
+  }
+}
