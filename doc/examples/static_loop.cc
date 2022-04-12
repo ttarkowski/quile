760fa67f50ae14e27fcd05b15fb3301fcce4c551
Tomasz Tarkowski <Tomasz.Tarkowski.TT@gmail.com>
2022-04-12 13:29:19 +0200

    static_loop example.

---
+++ doc/examples/static_loop.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <quile/quile.h>
+
+template<int N>
+struct test {
+  constexpr static int value = N;
+};
+
+int main() {
+  quile::static_loop<int, 0, 3>::body(
+    [=](auto I) {
+      using T = test<I>;
+      std::cout << T::value << std::endl;
+    }
+  );
+}
