192ee19b47ab041017ec5071806da631cc074797
Tomasz Tarkowski <Tomasz.Tarkowski.TT@gmail.com>
2022-04-12 14:17:38 +0200

    fn_or example.

---
+++ doc/examples/fn_or.cc
@@ -0,0 +1,10 @@
+#include <cassert>
+#include <quile/quile.h>
+
+int main() {
+  const auto f0 = [](int i) { return i == 42; };
+  const auto f1 = [](int i) { return i % 2 == 1; };
+  const auto f = quile::fn_or(f0, f1);
+  assert(f(3));
+  assert(f(42));
+}
