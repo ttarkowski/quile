b374e40322405f10de7b61ddccfa0d09524eafe0
Tomasz Tarkowski <Tomasz.Tarkowski.TT@gmail.com>
2022-04-19 14:56:04 +0200

    Fix.

--- doc/examples/cart2spher.cc
+++ doc/examples/cart2spher.cc
@@ -10,8 +10,8 @@ main()
   std::cout << "(x, y, z) = (" << x << ", " << y << ", " << z << ")\n";
 
   const auto [r, t, p] = quile::cart2spher(x, y, z);
-  std::cout << "(r, theta, phi) = " << r << ", " << t << ", " << p << ")\n";
+  std::cout << "(r, theta, phi) = (" << r << ", " << t << ", " << p << ")\n";
 
   const auto [x2, y2, z2] = quile::spher2cart(r, t, p);
-  std::cout << "(x, y, z) = " << x2 << ", " << y2 << ", " << z2 << ")\n";
+  std::cout << "(x, y, z) = (" << x2 << ", " << y2 << ", " << z2 << ")\n";
 }
